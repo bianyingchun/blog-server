ad21b628c716f2435b12928449441603f85ee254
bianyingchun <2093213209@qq.com>
2020-04-25 22:33:37 +0800

    x

---
+++ app.js
@@ -0,0 +1,20 @@
+const Koa = require("koa");
+const Router = require("koa-router");
+const fs = require("fs");
+const path = require("path");
+const cors = require("koa2-cors");
+
+const router = Router();
+const app = new Koa();
+
+const PORT = 3030;
+
+// static
+// app.use(koaStatic(path.resolve(__dirname, "../build")));
+
+// routes
+app.use(router.routes());
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+});
